2069883759b4887d90da0620f4b81cb67da8bc13
danield <daniel@diamand.org>
2023-06-29 16:10:25 +0100

    Setup main for lexer

--- src/main/java/com/myfirstlanguage/mfl/Mfl.java
+++ src/main/java/com/myfirstlanguage/mfl/Mfl.java
@@ -1,5 +1,43 @@
 package com.myfirstlanguage.mfl;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Scanner;
+
+import com.myfirstlanguage.mfl.lexer.Token;
+import com.myfirstlanguage.mfl.lexer.Lexer;
+
 public class Mfl {
-     
+    static boolean hadError = false;
+    static boolean hadRuntimeError = false;
+
+    public static void main(String[] args) throws IOException {
+        runPrompt();
+    }
+
+    //This is how to REPL works
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null)
+                break;
+            run(line);
+            hadError = false;
+        }
+    }
+
+    //The heart of our code
+    private static void run(String source) {
+        Lexer lexer = new Lexer(source);
+        List<Token> tokens = lexer.scanTokens();
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
 }
